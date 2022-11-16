d4760f40231f3958b0689901448551799f0d7ec3
Space Banana <98528301+spacebanana420@users.noreply.github.com>
2022-11-16 10:44:57 +0000

    Update doremisecure.sh

--- doremisecure.sh
+++ doremisecure.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #------Config------
-encryption=zip #zip, gpg, openssl (gpg to be added)
+encryption=openssl #zip, gpg, openssl (gpg to be added)
 format_zip=doremyzip
 format_openssl=doremy
 zip_level=0 #0-9, 0 recommended to disable compression
 cipher=aes256 # "openssl enc -ciphers" for a full list of available ciphers
-folder="Secured Folder" # If the folder name is "current", the script will analyze the current directory instead
+folder="Safe Folder" # If the folder name is "current", the script will analyze the current directory instead
 #------------------
 
 function encrypt ()  {
@@ -22,7 +22,13 @@ gpg)
 openssl)
     if [[ $1 != ".$format_openssl" ]]
     then
-        openssl enc -e -in "$1" -k "$2" -pbkdf2 -$cipher -out "$1.$format_openssl"
+        if [[ -d $1 ]]
+        then
+            zip -0 -r -q "$1.zip" "$1"
+            openssl enc -e -in "$1.zip" -k "$2" -pbkdf2 -$cipher -out "$1.zip.$format_openssl"
+        else
+            openssl enc -e -in "$1" -k "$2" -pbkdf2 -$cipher -out "$1.$format_openssl"
+        fi
     fi
 ;;
 esac
@@ -33,7 +39,7 @@ case $encryption in
 zip)
     if [[ $1 == ".$format_zip" ]]
     then
-        unzip -P "$2" $1
+        unzip -P "$2" "$1"
     fi
 ;;
 gpg)
@@ -43,6 +49,10 @@ openssl)
     then
         noformat=$(basename "$1" ".$format_openssl")
         openssl enc -d -in "$1" -k "$2" -pbkdf2 -$cipher -out "$noformat"
+        if [[ $1 == *".zip"* ]]
+        then
+            unzip "$noformat"
+        fi
     fi
 ;;
 esac
@@ -76,7 +86,7 @@ e)
     done
 ;;
 *)
-        echo "Secure Folder Version 0.4"
+        echo "Secure Folder Version 0.5"
         echo "Usage: securefolder.sh [option]"
         echo "Options:"
         echo "h      Help documentation"; echo "e      Encrypt a file/folder"; echo "d      Decrypt a file/folder"
